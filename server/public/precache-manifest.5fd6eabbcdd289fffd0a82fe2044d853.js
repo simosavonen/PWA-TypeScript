6e83c7c27fcfb98c7df578a6ebfb75691441c564
Simo Savonen <sipesa@utu.fi>
2019-05-21 21:08:03 +0300

    include fresh build of the frontend

---
+++ server/public/precache-manifest.5fd6eabbcdd289fffd0a82fe2044d853.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed16e9177be086798cf0703da3014f00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9dd566a83085525cf99",
+    "url": "/static/css/2.62908225.chunk.css"
+  },
+  {
+    "revision": "436879bb86a13fb60e07",
+    "url": "/static/css/main.88824ec2.chunk.css"
+  },
+  {
+    "revision": "a9dd566a83085525cf99",
+    "url": "/static/js/2.03b707b8.chunk.js"
+  },
+  {
+    "revision": "436879bb86a13fb60e07",
+    "url": "/static/js/main.df497736.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- server/public/service-worker.js
+++ server/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4890f0989723097c0c6988548626b17b.js"
+  "/precache-manifest.5fd6eabbcdd289fffd0a82fe2044d853.js"
 );
 
 self.addEventListener('message', (event) => {
