eaef50d486172e9d8f3950f1bf419eb0bc877e40
Simo Savonen <sipesa@utu.fi>
2019-05-21 12:54:35 +0300

    tsconfig tweaking

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
+
 import * as express from 'express'
 import * as bodyParser from 'body-parser'
 import * as fetch from 'node-fetch'
