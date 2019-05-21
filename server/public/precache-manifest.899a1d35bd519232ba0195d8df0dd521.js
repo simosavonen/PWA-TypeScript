3f3fbf40d7ed5fb2a07f6d8f88978665af75c123
Simo Savonen <sipesa@utu.fi>
2019-05-21 13:15:35 +0300

    semi automatic deployment of react frontend

---
+++ server/public/precache-manifest.899a1d35bd519232ba0195d8df0dd521.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba0dd62febb3c8c4bb6591ac8b4298e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b61a547d0bdfafe0451",
+    "url": "/static/css/2.62908225.chunk.css"
+  },
+  {
+    "revision": "5e474b3865f9adbb0c71",
+    "url": "/static/css/main.88824ec2.chunk.css"
+  },
+  {
+    "revision": "7b61a547d0bdfafe0451",
+    "url": "/static/js/2.93ed5dd5.chunk.js"
+  },
+  {
+    "revision": "5e474b3865f9adbb0c71",
+    "url": "/static/js/main.f3bf6fa6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
